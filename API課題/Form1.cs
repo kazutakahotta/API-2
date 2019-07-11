b6d1100222d5f9e49f56639b2de5f8ecba888ae5
HOTTAK <kazutaka.hotta@paloma.co.jp>
2019-07-11 22:50:35 +0900

    中身追加

--- API課題/Form1.cs
+++ API課題/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RestSharp;
 
 namespace API課題
 {
@@ -15,6 +16,32 @@ namespace API課題
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string code = textBox1.Text;
+            var client = new RestClient();
+            var request = new RestRequest();
+
+
+            client.BaseUrl = new Uri("http://zipcloud.ibsnet.co.jp/api/search");
+
+            request.Method = Method.GET;
+            request.AddParameter("zipcode", code, ParameterType.GetOrPost);
+            var response = client.Execute(request);
+            string json = response.Content.ToString();
+            var t = Newtonsoft.Json.JsonConvert.DeserializeObject<Postzip>(json);
+
+            if (t.status != "200")
+            {
+                textBox2.Text = t.message;
+            }
+            else
+            {
+                textBox2.Text = t.results[0].address1 + t.results[0].address2 + t.results[0].address3;
+            }
+            
         }
     }
 }
---
+++ API課題/zipcode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API課題
+{
+     public class Postzip
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+        public results[] results { get; set; }
+       }
+
+    public class results
+    {
+        public string zipcode { get; set; }
+        public string prefcode { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string address3 { get; set; }
+        public string kana1 { get; set; }
+        public string kana2 { get; set; }
+        public string kana3 { get; set; }
+    }
+
+}
+	
